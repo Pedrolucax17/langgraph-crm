610e43d782844f52888cd6280a9cc5ec540cb1d4
Pedro Lucas <pl.lucassouza@gmail.com>
2026-07-22 10:44:07 -0300

    feat: add SQL drop_crm_tables for database schema setup

---
+++ sql/00_drop_crm_tables.sql
@@ -0,0 +1,6 @@
+drop table if exists public.itens_proposta cascade;
+drop table if exists public.propostas cascade;
+drop table if exists public.tarefas_lead cascade;
+drop table if exists public.notas_lead cascade;
+drop table if exists public.leads cascade;
+drop table if exists public.status_lead cascade;
